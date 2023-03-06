8e66d592774e1b390d2b7a33e79c0e9dfde740f9
Silas Hsu <hsu.silas@wustl.edu>
2023-03-06 00:29:12 -0600

    Added tab closing instructions

--- frontend/src/components/EndScreen.tsx
+++ frontend/src/components/EndScreen.tsx
@@ -4,6 +4,12 @@ import { CopyButton } from "./CopyButton";
 import "./EndScreen.css";
 
 export function EndScreen() {
+    const [isHelpOpen, setIsHelpOpen] = React.useState(false);
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isIOS = detectIOS(userAgent);
+    const isAndroid = userAgent.includes("android");
+    const isMobile = isIOS || isAndroid;
+
     return <div className="EndScreen EndScreen-fade-in vertical-center">
         <div className="container">
             <h1>Thanks for browsing!</h1>
@@ -12,9 +18,31 @@ export function EndScreen() {
                 <code>{process.env.REACT_APP_CONTINUE_CODE || ""}</code>
                 <CopyButton copyString={process.env.REACT_APP_CONTINUE_CODE || ""} />
             </p>
-            <button className="btn btn-link" onClick={() => window.history.back()}>
-                {"<"} Back to Qualtrics
-            </button>
+            <p className="EndScreen-instructions">
+                You may now close the current tab and return to Qualtrics.
+                {isMobile && !isHelpOpen && <button className="btn btn-link" onClick={() => setIsHelpOpen(true)}>
+                    How do I manage tabs?
+                </button>}
+            </p>
+
+            <div style={{ marginTop: "2em", visibility: isHelpOpen ? "visible" : "hidden" }}>
+                {isIOS && <p>
+                    To manage tabs, tap Switch Tabs on the right side of the address bar. It probably looks like a
+                    square overlapping another square.  (You may also have to tap the address bar first to open up the
+                    controls.)
+                </p>}
+                {isAndroid && <p>
+                    To manage tabs, tap Switch Tabs on the right side of the address bar.  It probably looks like
+                    a square with a number inside it.
+                </p>}
+            </div>
         </div>
     </div>;
+}
+
+function detectIOS(userAgent: string) {
+    if (userAgent.includes("iphone")) {
+        return true;
+    }
+    return (userAgent.match(/(ipad)/) || (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1));
 }
